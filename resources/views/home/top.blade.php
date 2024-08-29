4b676ae03fe4adea2e335d62eb16b000290ff763
banskotanup <baskota.anup11@gmail.com>
2024-08-29 21:53:57 +0545

    Fix banner page design and other design as well
    Fix banner page welcome banner and change agriculture farm into a quotes and fix the css design of the page
    Changes:
    	modified:   public/css/style.css
    	modified:   resources/views/home/top.blade.php

--- resources/views/home/top.blade.php
+++ resources/views/home/top.blade.php
@@ -5,9 +5,9 @@
                <div class="container">
                    <div class="row">
                        <div class="col-lg-7 order-lg-1 order-2 banner-content">
-                           <h2 class="text-uppercase position-relative">Hell0</h2>
-                           <h1 class="text-uppercase">This is Rong Krishi</h1>
-                           <h5 class="text-uppercase">Agricultural Goods Manufacturarers</h5>
+                           <h2 class="text-uppercase position-relative">Welcome </h2>
+                           <h1 class="text-uppercase" style="font-size: 35px;">Rong Krishi Company Pvt. Ltd.</h1>
+                           <h5 class="text-uppercase">fresh grains, dairy delights, and packaged goods</h5>
                            <div class="mt-5">
                                <a class="main-btn primary-btn" href="#">Explore</a>
                                <a class="main-btn secondary-btn ms-4" href="#">Watch Video</a>
@@ -15,7 +15,7 @@
                        </div>
                        <div class="col-lg-5 order-lg-2 order-1">
                            <div class="top-right-img">
-                               <img src="/images/banner3.jpg" class="img-fluid">
+                               <img src="/images/banner3.jpg" class="img-fluid" style="opacity: 0.75;">
                            </div>
                        </div>
                    </div>
@@ -27,13 +27,13 @@
                <div class="container">
                    <div class="row align-items-center">
                        <div class="col-lg-7 mb-4 mb-lg-0">
-                           <img src="images/about-us.webp" class="img-fluid" alt="about-us">
+                           <img src="images/banner1.jpg" class="img-fluid" alt="about-us" style="opacity: 0.85;">
                        </div>
                        <div class="col-lg-5 mb-4 mb-lg-0">
                            <h3>let’s <br>Introduce us</h3>
                            <p>At RongKrishi, we are dedicated to driving innovation in the agricultural sector through cutting-edge manufacturing solutions. Our commitment to excellence ensures that farmers have access to high-quality, reliable equipment that enhances productivity and sustainability.</p>
                            <p> Our state-of-the-art manufacturing facilities are equipped with advanced technology to produce durable and high-performance agricultural tools. We are dedicated to providing exceptional customer support, ensuring that our clients receive timely assistance and solutions tailored to their specific needs. By partnering with us, farmers can rely on a trusted leader in agricultural manufacturing to enhance their operations and drive success.</p>
-                           <a href="Anup_Banskota_Resume.pdf" class="main-btn primary-btn mt-4">Learn More</a>
+                           <a href="#" class="main-btn primary-btn mt-4">Learn More</a>
                        </div>
                    </div>
                    <div class="row justify-content-center pt-5">
@@ -89,8 +89,8 @@
                        <div class="offset-lg-2 col-lg-4 col-md-10">
                            <div class="client-info">
                                <div class="d-flex">
-                                   <span class="large">04</span>
-                                   <span class="smll">Years <br>Experience <br>Working</span>
+                                   <span class="large">01</span>
+                                   <span class="smll">Year <br>INTO <br>Working</span>
                                </div>
                                <div class="call-now d-flex py-4">
                                    <div>
@@ -98,7 +98,7 @@
                                    </div>
                                    <div class="ms-4">
                                        <p>Call us now</p>
-                                       <h5>(+977)9804911528</h5>
+                                       <h5>(+977) 9876543210</h5>
                                    </div>
                                </div>
                            </div>
@@ -110,7 +110,7 @@
                               <div class="row">
                                  <div class="col-md-12">
                                     <div class="willom">
-                                      <h1> Agriculture Fram</h1>
+                                      <h1><em>"From farm to table, every vegetable tells a story of care and cultivation."</em></h1>
                                     </div>
                                  </div>
                               </div>
